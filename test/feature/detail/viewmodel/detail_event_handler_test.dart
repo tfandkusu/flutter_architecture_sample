b075ef9654f593f6e2ded7dbc7df37a169d66c39
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-09-27 22:06:46 +0900

    DetailEventHandler#onCreateのテストを作成

--- test/feature/detail/viewmodel/detail_event_handler_test.dart
+++ test/feature/detail/viewmodel/detail_event_handler_test.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_architecture_sample/catalog/github_repo_catalog.dart';
 import 'package:flutter_architecture_sample/data/repository/github_repo_repository.dart';
 import 'package:flutter_architecture_sample/data/repository/github_repo_repository_provider.dart';
 import 'package:flutter_architecture_sample/feature/detail/viewmodel/detail_event_handler_provider.dart';
+import 'package:flutter_architecture_sample/feature/detail/viewmodel/detail_ui_model_state_notifier.dart';
+import 'package:flutter_architecture_sample/feature/detail/viewmodel/detail_ui_model_state_notifier_provider.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/annotations.dart';
@@ -8,8 +11,29 @@ import 'package:mockito/mockito.dart';
 import 'detail_event_handler_test.mocks.dart';
 
 // モック実装をMockitoに作らせる設定
-@GenerateNiceMocks([MockSpec<GithubRepoRepository>()])
+@GenerateNiceMocks(
+    [MockSpec<GithubRepoRepository>(), MockSpec<DetailUiModelStateNotifier>()])
 void main() {
+  // 画面が開かれた
+  test("DetailEventHandler#onCreate", () async {
+    // 依存するインスタンスのモック実装を作成する
+    final stateNotifier = MockDetailUiModelStateNotifier();
+    final repository = MockGithubRepoRepository();
+    final repo = getGithubRepoCatalog()[0];
+    const readme = "# some_repository";
+    when(repository.getReadme(repo)).thenAnswer((_) => Future.value(readme));
+    final container = ProviderContainer(overrides: [
+      detailUiModelStateNotifierProvider.overrideWithValue(stateNotifier),
+      githubRepoRepositoryProvider.overrideWithValue(repository)
+    ]);
+    // テスト対象を取得
+    final eventHandler = container.read(detailEventHandlerProvider);
+    // テスト対象メソッドを呼び出し
+    await eventHandler.onCreate(repo);
+    verifyInOrder(
+        [repository.getReadme(repo), stateNotifier.setReadme(readme)]);
+  });
+
   // 「いいね」ボタンが押された
   test("DetailEventHandler#onClickFavorite", () async {
     // 依存するインスタンスのモック実装を作成する
