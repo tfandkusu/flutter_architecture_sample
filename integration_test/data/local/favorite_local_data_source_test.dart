1d10aedc1ce42e46ed4b007fe2a01f6a8193fd6f
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-11-06 23:05:13 +0900

    FavoriteLocalDataSourceについて、インテグレーションテストを追加。

---
+++ integration_test/data/local/favorite_local_data_source_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_architecture_sample/data/local/favorite_local_data_source_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// FavoriteLocalDataSourceを実機またはエミュレータで動かす
+void main() {
+  test("FavoriteLocalDataSource", () async {
+    final container = ProviderContainer();
+    final localDataSource = container.read(favoriteLocalDataSourceProvider);
+    // データをクリアする
+    await localDataSource.clear();
+    // 最初は何も「いいね」をしていない
+    expect(await localDataSource.getFavoriteRepoNameSet(), <String>{});
+    // flutter_architecture_sampleに「いいね」を付ける
+    localDataSource.setFavorite('flutter_architecture_sample', true);
+    // flutter_architecture_sampleだけのセットを取得する
+    expect(await localDataSource.getFavoriteRepoNameSet(),
+        <String>{'flutter_architecture_sample'});
+    // android_app_templateに「いいね」を付ける
+    localDataSource.setFavorite('android_app_template', true);
+    // さっき「いいね」を付けたflutter_architecture_sample
+    // android_app_templateのセットを取得する
+    expect(await localDataSource.getFavoriteRepoNameSet(),
+        <String>{'flutter_architecture_sample', 'android_app_template'});
+    // flutter_architecture_sampleから「いいね」を消す
+    localDataSource.setFavorite('flutter_architecture_sample', false);
+    // さっき「いいね」を付けたandroid_app_templateだけのセットを取得する
+    expect(await localDataSource.getFavoriteRepoNameSet(),
+        <String>{'android_app_template'});
+    // android_app_templateに多重に「いいね」をつける
+    localDataSource.setFavorite('android_app_template', true);
+    expect(await localDataSource.getFavoriteRepoNameSet(),
+        <String>{'android_app_template'});
+    // 存在しないリポジトリの「いいね」を消す
+    localDataSource.setFavorite('unknown', false);
+    expect(await localDataSource.getFavoriteRepoNameSet(),
+        <String>{'android_app_template'});
+  });
+}
