638691e7ee1d046ad44b14691104bc93f2f2e2a7
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-11-06 23:27:32 +0900

    clearメソッドのテスト

--- integration_test/data/local/favorite_local_data_source_test.dart
+++ integration_test/data/local/favorite_local_data_source_test.dart
@@ -12,27 +12,27 @@ void main() {
     // 最初は何も「いいね」をしていない
     expect(await localDataSource.getFavoriteRepoNameSet(), <String>{});
     // flutter_architecture_sampleに「いいね」を付ける
-    localDataSource.setFavorite('flutter_architecture_sample', true);
+    await localDataSource.setFavorite('flutter_architecture_sample', true);
     // flutter_architecture_sampleだけのセットを取得する
     expect(await localDataSource.getFavoriteRepoNameSet(),
         <String>{'flutter_architecture_sample'});
     // android_app_templateに「いいね」を付ける
-    localDataSource.setFavorite('android_app_template', true);
+    await localDataSource.setFavorite('android_app_template', true);
     // さっき「いいね」を付けたflutter_architecture_sample
     // android_app_templateのセットを取得する
     expect(await localDataSource.getFavoriteRepoNameSet(),
         <String>{'flutter_architecture_sample', 'android_app_template'});
     // flutter_architecture_sampleから「いいね」を消す
-    localDataSource.setFavorite('flutter_architecture_sample', false);
+    await localDataSource.setFavorite('flutter_architecture_sample', false);
     // さっき「いいね」を付けたandroid_app_templateだけのセットを取得する
     expect(await localDataSource.getFavoriteRepoNameSet(),
         <String>{'android_app_template'});
     // android_app_templateに多重に「いいね」をつける
-    localDataSource.setFavorite('android_app_template', true);
+    await localDataSource.setFavorite('android_app_template', true);
     expect(await localDataSource.getFavoriteRepoNameSet(),
         <String>{'android_app_template'});
     // 存在しないリポジトリの「いいね」を消す
-    localDataSource.setFavorite('unknown', false);
+    await localDataSource.setFavorite('unknown', false);
     expect(await localDataSource.getFavoriteRepoNameSet(),
         <String>{'android_app_template'});
   });
